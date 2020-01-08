4393370cbea4792b6963f041afed8ca3b31c537b
quentin chabot <quentin.chabot@outlook.fr>
2020-01-08 11:48:00 +0100

    crud user & header categories

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,16 +6,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
+
 
 class MainController extends AbstractController {
 
 	/**
 	 * @Route("/", name="main")
 	 */
-	public function index()
+	public function index(CategoryRepository $category_repository): Response
 	{
-		return $this->render('main/index.html.twig');
+	   return $this->render('main/index.html.twig',['categories' => $category_repository->findAll(),]);
 	}
-
-
 }

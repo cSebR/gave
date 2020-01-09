d47be0549f9ff8714706f4baee1e91e911c9dafd
quentin chabot <quentin.chabot@outlook.fr>
2020-01-09 17:28:16 +0100

    Workin Progress Categorie & Tag

--- src/Service/AppService.php
+++ src/Service/AppService.php
@@ -4,18 +4,26 @@ namespace App\Service;
 
 use App\Repository\CategoryRepository;
 use App\Repository\BookRepository;
+use App\Repository\UserRepository;
+use App\Repository\TagRepository;
 
 class AppService
 {
     private $category_repository;
     private $book_repository;
+    private $user_repository;
+    private $tag_repository;
 
     public function __construct(
         CategoryRepository $category_repository,
-        BookRepository $book_repository
+        BookRepository $book_repository,
+        UserRepository $user_repository,
+        TagRepository $tag_repository
     ) {
         $this->category_repository = $category_repository;
         $this->book_repository = $book_repository;
+        $this->user_repository = $user_repository;
+        $this->tag_repository = $tag_repository;
     }
 
     public function getCategories()
@@ -26,5 +34,15 @@ class AppService
     public function getBooks()
     {
 	   return $this->book_repository->findAll();
+    }
+
+    public function getUsers()
+    {
+	   return $this->user_repository->findAll();
+    }
+
+    public function getTags()
+    {
+	   return $this->tag_repository->findAll();
     }
 }

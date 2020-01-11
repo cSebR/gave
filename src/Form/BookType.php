4370360f8122b51649d89bc4df7d77fb74d78ff7
cSebR <caroen.sebastien@gmail.com>
2020-01-11 15:27:42 +0100

    crud livre

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -2,13 +2,22 @@
 
 namespace App\Form;
 
+
 use App\Entity\Book;
 use App\Entity\Category;
+use App\Entity\Collection;
+use App\Entity\Etat;
 use App\Entity\Format;
+use App\Entity\Language;
 use App\Entity\Publisher;
-use App\Repository\FormatRepository;
-use App\Repository\PublisherRepository;
+use App\Entity\Tag;
 use App\Repository\CategoryRepository;
+use App\Repository\CollectionRepository;
+use App\Repository\EtatRepository;
+use App\Repository\FormatRepository;
+use App\Repository\LanguageRepository;
+use App\Repository\PublisherRepository;
+use App\Repository\TagRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
@@ -24,7 +33,9 @@ class BookType extends AbstractType
             ->add('cover')
             ->add('backCover')
             ->add('title')
-            ->add('publishedDate')
+            ->add('publishedDate',DateTimeType::class, [
+	            'date_label' => 'Starts On'
+            ])
             ->add('numberOfCopies')
             ->add('priceHT')
             ->add('priceTTC')
@@ -42,25 +53,14 @@ class BookType extends AbstractType
             ->add('weight')
             //->add('isAvailable')
             //->add('createdAt')
-            //->add('authorId')
-            ->add('format',EntityType::class,[
-            	'class' => Format::class,
+            /*->add('author',EntityType::class,[
+	            'class' => Author::class,
 	            'attr' => ['class' => 'select'],
-	            'query_builder' => function(FormatRepository $fr){
-            	return $fr->createQueryBuilder('f');
+	            'query_builder' => function(AuthorRepository $pr){
+		            return $pr->createQueryBuilder('a');
 	            },
 	            'choice_label' => 'label'
-                ])
-            ->add('publisher',EntityType::class,[
-	            'class' => Publisher::class,
-	            'attr' => ['class' => 'select'],
-
-	            'query_builder' => function(PublisherRepository $pr){
-		            return $pr->createQueryBuilder('p');
-	            },
-	            'choice_label' => 'label'
-                ])
-            //->add('theme')
+            ])*/
             ->add('category',EntityType::class,[
 	            'class' => Category::class,
 	            'attr' => ['class' => 'select'],
@@ -69,7 +69,59 @@ class BookType extends AbstractType
 	            },
 	            'choice_label' => 'label'
             ])
+            ->add('format',EntityType::class,[
+	            'class' => Format::class,
+	            'attr' => ['class' => 'select'],
+	            'query_builder' => function(FormatRepository $fr){
+		            return $fr->createQueryBuilder('f');
+	            },
+	            'choice_label' => 'label'
+            ])
+            ->add('publisher',EntityType::class,[
+	            'class' => Publisher::class,
+	            'attr' => ['class' => 'select'],
+
+	            'query_builder' => function(PublisherRepository $pr){
+		            return $pr->createQueryBuilder('p');
+	            },
+	            'choice_label' => 'label'
+            ])
+            /*->add('collection',EntityType::class,[
+	            'class' => Collection::class,
+	            'attr' => ['class' => 'select'],
+	            'query_builder' => function(CollectionRepository $pr){
+		            return $pr->createQueryBuilder('co');
+	            },
+	            'choice_label' => 'label'
+            ])*/
+            ->add('etat',EntityType::class,[
+	            'class' => Etat::class,
+	            'attr' => ['class' => 'select'],
+	            'query_builder' => function(EtatRepository $pr){
+		            return $pr->createQueryBuilder('e');
+	            },
+	            'choice_label' => 'label'
+            ])
+            ->add('language',EntityType::class,[
+	            'class' => Language::class,
+	            'attr' => ['class' => 'select'],
+	            'query_builder' => function(LanguageRepository $pr){
+		            return $pr->createQueryBuilder('l');
+	            },
+	            'choice_label' => 'label'
+            ])
+
+            /*->add('tags',EntityType::class,[
+	            'class' => Tag::class,
+	            'attr' => ['class' => 'select'],
+	            'query_builder' => function(TagRepository $pr){
+		            return $pr->createQueryBuilder('t');
+	            },
+	            'choice_label' => 'label'
+            ])*/
         ;
+
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
